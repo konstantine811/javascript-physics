2867839e42dbe29b8748c335f57f36804520c514
Abramov Constantine <k.abramkin@kernel.ua>
2021-07-06 03:18:28 +0300

    first commit

--- src/code/index.ts
+++ src/code/index.ts
@@ -2,11 +2,4 @@ document.addEventListener('DOMContentLoaded', ready);
 
 function ready() {
   const prelaodEl = document.querySelector('.full-preloader');
-  const logoHeader = document.getElementById('header-logo');
-  logoHeader.style.opacity = '0';
-  setTimeout(() => {
-    prelaodEl.classList.add('fade-out');
-    logoHeader.style.opacity = '1';
-    logoHeader.classList.add('logo-start-anim');
-  }, 1000);
 }
